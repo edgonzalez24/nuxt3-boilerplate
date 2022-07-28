24b435419de10febc3cbfaf0509507293f1ffab9
Edwin <edgoca2498@gmail.com>
2022-07-28 15:31:32 -0600

    Initial commit

---
+++ components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+  <nav class="my-5">
+    <div class="container mx-auto">
+      <p> Navbar / Header</p>
+    </div>
+  </nav>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="container mx-auto min-h-screen">
+    <h1 class="text-2xl font-bold font-sans">Content</h1>
+  </div>
+</template>
